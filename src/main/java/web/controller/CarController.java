5e32cac5046156ccdc8fec4c200d43acabca1794
AleksandrGromov99 <163206082+AleksandrGromov99@users.noreply.github.com>
2024-04-08 23:58:15 +0300

    Update CarController.java

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,9 +3,7 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
@@ -22,19 +20,9 @@ public class CarController {
         this.carService = carService;
     }
 
-//    @GetMapping("/cars")
-//    public String show(@PathVariable("id") int id, Model model) {
-//        model.addAttribute("cars", carService.show(id));
-//        return "/cars";
-//    }
-
     @GetMapping("")
-    public String show(@RequestParam(value = "count", required = false) Integer count, Model model) {
-        if (count!=null){
-            model.addAttribute("cars", carService.show(count));
-        } else {
-            model.addAttribute("cars",carService.show(10));
-        }
+    public String show(@RequestParam(defaultValue = "5") Integer count, Model model) {
+        model.addAttribute("cars", carService.show(count));
         return "/cars";
     }
 }
